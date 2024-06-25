e208f24bbed31f3142e6f896cad777370d59fabe
Joseph <joseph.riveracosta@csedge.org>
2024-06-25 13:50:25 -0700

    Mad lib creation

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,17 @@
 #Madlib by Joseph
-
 print("Here is my first commit :()")
+
+verb = input("enter a verb:")
+adjective = input("enter an adjective:")
+adjective2 = input("enter an adjective:")
+noun = input("enter a noun:")
+adjective3 = input("enter an adjective:")
+verb2 = input("enter a verb:")
+adverb = input("enter an adverb:")
+
+print(f"In the future, robots will be able to {verb} alongside humans, performing tasks with {adjective} precision and efficiency. These robots will have {adjective2} artificial intelligence, allowing them to think and learn just like humans. They will roam the streets, interacting with {noun} and helping to create a more {adjective3} society. However, not everyone will {verb2} welcome these new robotic companions. Some may {adverb} fear the unknown, while others embrace the future with open arms.")
+
+
+
+
+
